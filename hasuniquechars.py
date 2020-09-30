4ebb2e7d7234e33323d001df9be5b2c52b0ed0b4
Iryna Loza <irynloza@gmail.com>
2020-09-29 20:58:45 -0700

    Complete has_unique_chars

--- hasuniquechars.py
+++ hasuniquechars.py
@@ -21,6 +21,21 @@ Uppercase and lowercase letters should be considered separately::
 def has_unique_chars(word):
     """Does word contains unique set of characters?"""
 
+    if len(word) == 0:
+        return True 
+
+    chars = {}
+
+    for char in word:
+        if char not in chars:
+            chars[char] = 0
+        chars[char]+= 1 
+
+    for key in chars:
+        if chars[key] > 1:
+            return False
+    return True             
+
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
